05fba444773c323cd4ebbd7fef366a8e998f759f
Manali <manali.coursera.com>
2016-09-23 02:11:32 -0700

    week1 assignment done

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,11 +3,39 @@
 
 angular.module('LunchCheck', [])
 
-.controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
-  $scope.sayHello = function () {
-    return "Hello Coursera!";
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.inject = [$scope, $filter];
+
+function LunchCheckController($scope, $filter) {
+  $scope.lunchItems = "";
+  $scope.lunchItemCount = 0;
+  $scope.lunchMessage = "";
+
+  function splitString(stringToSplit, separator) {
+    var arrayOfStrings = stringToSplit.split(separator);
+    $scope.lunchItemCount = arrayOfStrings.length;
+
+    if (stringToSplit.length == 0) {
+      $scope.lunchMessage = "Please enter data first";
+      return;
+    }
+
+    if ($scope.lunchItemCount==0) {
+      $scope.lunchMessage = "Please enter data first";
+    } else if ($scope.lunchItemCount < 4) {
+      $scope.lunchMessage = "Enjoy!";
+    } else {
+      $scope.lunchMessage = "Too Much!";
+    }
   };
-});
+
+$scope.displayLunchResult = function() {
+  splitString($scope.lunchItems, ',');
+};
+
+
+
+};
 
 })();
